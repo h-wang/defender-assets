6039f852ff3aa9c88ea47b7bf3f879c1c5753223
Hongliang <git@hongliang.nl>
2016-03-29 12:32:56 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # defender-assets
-Defender rules
+Defender rules for [Defender](https://github.com/h-wang/defender).
+This way you can customize your own defender rules.
+
+## How to use
+```php
+use Hongliang\Defender\Defender;
+use Hongliang\Defender\Voter\IpRangeVoter;
+use Hongliang\Defender\Voter\UriKeywordVoter;
+
+$defender = new Defender();
+
+$file = '[path to defender-assets/assets.php';
+if (file_exists($file)) {
+    $assets = require_once($file);
+
+    $keywordVoter = new UriKeywordVoter();
+    $keywordVoter->setAssets($assets['urikeywords']);
+
+    $ipVoter = new IpRangeVoter();
+    $ipVoter->setAssets($assets['iprange']);
+
+    $defender->addVoter($ipVoter, Defender::FORBIDDEN)
+        ->addVoter($keywordVoter, Defender::DENY);
+} else {
+    $defender->addVoter(new IpRangeVoter(), Defender::FORBIDDEN)
+        ->addVoter(new UriKeywordVoter(), Defender::DENY);
+}
+$defender->react();
+```
---
+++ assets.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'urikeywords' => [
+        'fckedit',
+        '/administrator/',
+        '/wp-',
+        'xmlrpc',
+        '/filemanager',
+        '/bbs',
+        '/convert',
+        '/product',
+        '/plus',
+        'whitelist.pac',
+    ],
+    'iprange' => [
+        ['59.62.0.0', '59.63.255.255'], // CHINANET JIANGXI PROVINCE NETWORK
+        ['59.172.0.0', '59.173.255.255'], // CHINANET Hubei province network
+        ['61.146.178.0', '61.146.178.255'], // MAINT-CHINANET-GD
+        ['62.210.0.0', '62.210.255.255'], // IE-POOL-BUSINESS-HOSTING Franch
+        ['86.123.0.0', '86.123.255.255'], // RDS Residential ROMANIA
+        ['89.122.0.0', '89.122.255.255'], // Romtelecom Data Network Romania
+        ['106.4.0.0', '106.7.255.255'], // CHINANET JIANGXI PROVINCE NETWORK
+        ['115.148.0.0', '115.254.255.255'], // many attempts, just block them all
+        ['117.21.0.0', '117.21.255.255'], // CHINANET JIANGXI PROVINCE NETWORK
+        ['117.41.0.0', '117.41.255.255'], // CHINANET JIANGXI PROVINCE NETWORK
+        ['146.148.128.0', '146.148.255.255'], // GCHAO LLC
+        ['148.251.0.0', '148.251.255.255'], // Hetzner Online GmbH
+        ['178.239.165.0', '178.239.165.255'], // COBALT-NETWORKS English
+        ['182.96.0.0', '182.111.255.255'], // CHINANET JIANGXI PROVINCE NETWORK
+        ['218.64.0.0', '218.65.127.255'], // CHINANET JIANGXI PROVINCE NETWORK
+        ['202.75.32.0', '202.75.63.254'], // TM VADS DC Hosting
+        ['222.208.0.0', '222.215.255.255'], // CHINANET Sichuan province network
+    ],
+];
